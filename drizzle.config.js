d1119ea2e3af165aa8a59ddb7d1d62e6952e8e2c
shubhankarshady <shubhankarg81@gmail.com>
2026-01-11 13:51:14 +0530

    remove hardcoded db url

--- drizzle.config.js
+++ drizzle.config.js
@@ -4,6 +4,6 @@ export default{
     schema : "./utils/schema.js",
     dialect: "postgresql",
     dbCredentials: {
-    url:  'postgresql://neondb_owner:npg_CL79hWtlYgqT@ep-hidden-term-ah1lgydy-pooler.c-3.us-east-1.aws.neon.tech/Interview_Prep?sslmode=require&channel_binding=require',
+    url: process.env.NEXT_DRIZZLE_DB_URL ,
   }
 }
